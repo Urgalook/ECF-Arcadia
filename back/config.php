f68b5cdcbca5a0c1b0a4e3757e741fcb0c19518f
Urgalook <maxime.bloise@outlook.com>
2024-05-20 14:53:00 +0200

    ajout bdd heroku

--- back/config.php
+++ back/config.php
@@ -1,4 +1,14 @@
 <?php
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+
 define("_DOMAIN_", ".ecfarcadiatest.local");
 define("_JUNGLE_IMAGES_FOLDER_", "/assets/images/Jungle/");
 define("_SAVANE_IMAGES_FOLDER_", "/assets/images/Savane/");
