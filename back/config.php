dab62980b3ae7a5828024b8736baa77d5a842e45
Urgalook <maxime.bloise@outlook.com>
2024-05-20 15:12:37 +0200

    first commit

--- back/config.php
+++ back/config.php
@@ -1,13 +1,11 @@
 <?php
-$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$cleardb_server = $cleardb_url["host"];
-$cleardb_username = $cleardb_url["user"];
-$cleardb_password = $cleardb_url["pass"];
-$cleardb_db = substr($cleardb_url["path"],1);
-$active_group = 'default';
-$query_builder = TRUE;
-// Connect to DB
-$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+$url = getenv('JAWSDB_URL');
+$dbparts = parse_url($url);
+
+$hostname = $dbparts['host'];
+$username = $dbparts['user'];
+$password = $dbparts['pass'];
+$database = ltrim($dbparts['path'],'/');
 
 define("_DOMAIN_", ".ecfarcadiatest.local");
 define("_JUNGLE_IMAGES_FOLDER_", "/assets/images/Jungle/");
