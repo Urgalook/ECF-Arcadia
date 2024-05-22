5858a78f2898c9d8f17f23f5f5a060c844357798
Urgalook <156817120+Urgalook@users.noreply.github.com>
2024-05-22 15:03:25 +0200

    Update service.php

--- admin/service.php
+++ admin/service.php
@@ -99,50 +99,46 @@ if (isset($_POST['Service'])) {
 
 ?>
 
-<div class="services-empl">
-    <div class="admin-empl d-flex flex-column flex-shrink-0 p-3 text-bg-dark" style="width: 350px; height: 40vh" >
-      <span class="fs-4">Menu</span>
-        <hr>
-        <ul class="nav nav-pills flex-column mb-auto">
+<div class="empl">
+  <div class="admin d-flex flex-column flex-shrink-0 p-3 text-bg-dark" style="width: 350px;">
+        <span class="fs-4">Menu</span>
+      <hr>
+      <ul class="nav nav-pills flex-column mb-auto">
         <li class="nav-item">
-            <a href="/employe/index.php" class="nav-link" aria-current="page">
+        <a href="/admin/index.php" class="nav-link" aria-current="page">
             <svg class="bi pe-none me-2" width="16" height="16"><use xlink:href="#home"></use></svg>
             Accueil
-            </a>
-        </li>
-        <li class="nav-item">
-            <a href="/employe/avis.php" class="nav-link" aria-current="page">
+          </a>
+          <a href="/admin/services.php" class="nav-link" aria-current="page">
             <svg class="bi pe-none me-2" width="16" height="16"><use xlink:href="#home"></use></svg>
-            Avis
-            </a>
-        </li>
-        <li>
-            <a href="/employe/animaux.php" class="nav-link text-white">
-            <svg class="bi pe-none me-2" width="16" height="16"><use xlink:href="#speedometer2"></use></svg>
-            Alimentation
-            </a>
-        </li>
-        <li>
-            <a href="/employe/services.php" class="nav-link text-white">
-            <svg class="bi pe-none me-2" width="16" height="16"><use xlink:href="#speedometer2"></use></svg>
             Services
-            </a>
+          </a>
         </li>
-        </ul>
-        <hr>
-    <div class="dropdown">
-      <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-        <img src="https://github.com/mdo.png" alt="" width="32" height="32" class="rounded-circle me-2">
-        <strong>mdo</strong>
-      </a>
-      <ul class="dropdown-menu dropdown-menu-dark text-small shadow">
-        <li><a class="dropdown-item" href="#">New project...</a></li>
-        <li><a class="dropdown-item" href="#">Settings</a></li>
-        <li><a class="dropdown-item" href="#">Profile</a></li>
-        <li><hr class="dropdown-divider"></li>
-        <li><a class="dropdown-item" href="#">Sign out</a></li>
+        <li>
+          <a href="/admin/horaires.php" class="nav-link text-white">
+            <svg class="bi pe-none me-2" width="16" height="16"><use xlink:href="#speedometer2"></use></svg>
+            Horaires
+          </a>
+        </li>
+        <li>
+          <a href="/admin/habitats.php" class="nav-link text-white">
+            <svg class="bi pe-none me-2" width="16" height="16"><use xlink:href="#table"></use></svg>
+            Habitats
+          </a>
+        </li>
+        <li>
+          <a href="/admin/animaux.php" class="nav-link text-white">
+            <svg class="bi pe-none me-2" width="16" height="16"><use xlink:href="#grid"></use></svg>
+            Animaux
+          </a>
+        </li>
+        <li>
+          <a href="/admin/veterinaire.php" class="nav-link text-white">
+            <svg class="bi pe-none me-2" width="16" height="16"><use xlink:href="#people-circle"></use></svg>
+            Compte-rendus vétérinaires
+          </a>
+        </li>
       </ul>
-    </div>
   </div>
     <div class="form-empl">
         <h1>Modification service</h1>
